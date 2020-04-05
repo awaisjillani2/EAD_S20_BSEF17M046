0d8bb1eea83daaccd2353fe9952ad154d4869f83
Awais Jillani <50237219+awaisjillani2@users.noreply.github.com>
2020-04-05 21:08:14 +0500

    Add files via upload

---
+++ Assignment2.DAL/AdminDAO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.DAL
+{
+    public static class AdminDAO
+    {
+        public static Boolean validateAdmin(String adminLogin, String adminPassword)
+        {
+            try
+            {
+                using (DBHelper helper = new DBHelper())
+                {
+                    String query;
+                    query = String.Format("Select count(*) from dbo.Admin where AdminLogin='{0}' and AdminPassword = '{1}'", adminLogin, adminPassword);
+                    var id = (int)helper.ExecuteScalar(query);
+                    if (id == 1)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static DataTable getUserData()
+        {
+            using (DBHelper helper = new DBHelper())
+            {
+                DataTable table = new DataTable("User");
+                DataColumn columnID = new DataColumn("UserID", typeof(System.Int32));
+                table.Columns.Add(columnID);
+                DataColumn columnName = new DataColumn("UserName", typeof(System.String));
+                table.Columns.Add(columnName);
+                DataColumn columnLogin = new DataColumn("UserLogin", typeof(System.String));
+                table.Columns.Add(columnLogin);
+                DataColumn columnAddress = new DataColumn("UserAddress", typeof(System.String));
+                table.Columns.Add(columnAddress);
+                DataColumn columnAge = new DataColumn("UserAge", typeof(System.Int32));
+                table.Columns.Add(columnAge);
+                String query;
+                query = "Select UserID, UserName, UserLogin, UserAddress, UserAge from dbo.Users";
+                var reader = helper.ExecuteReader(query);
+                while (reader.Read())
+                {
+                    DataRow row = table.NewRow();
+                    row["UserID"] = reader.GetInt32(0);
+                    row["UserName"] = reader.GetString(1);
+                    row["UserLogin"] = reader.GetString(2);
+                    row["UserAddress"] = reader.GetString(3);
+                    row["UserAge"] = reader.GetInt32(4);
+                    table.Rows.Add(row);
+                }
+                return table;
+            }
+        }
+    }
+}
---
+++ Assignment2.DAL/DBHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment2.DAL
+{
+    internal class DBHelper : IDisposable
+    {
+        String conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Amir\source\repos\Assignment2\Assignment2.mdf;Integrated Security = True";
+        SqlConnection con = null;
+
+        public DBHelper()
+        {
+            con = new SqlConnection(conStr);
+            con.Open();
+        }
+
+        public int ExecuteNonQuery(String query)
+        {
+            SqlCommand command = new SqlCommand(query, con);
+            var count = command.ExecuteNonQuery();
+            return count;
+        }
+
+        public Object ExecuteScalar(String query)
+        {
+            SqlCommand command = new SqlCommand(query, con);
+            return command.ExecuteScalar();
+        }
+
+        public SqlDataReader ExecuteReader(String query)
+        {
+            SqlCommand command = new SqlCommand(query, con);
+            return command.ExecuteReader();
+        }
+
+        public void Dispose()
+        {
+            if (con != null && con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
